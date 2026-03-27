38beb10d3c44b12a9e86c955010948c5340605d6
Jose Acosta <laja158@gmail.com>
2026-03-26 20:49:22 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pragma-todo',
+  webDir: 'www'
+};
+
+export default config;
