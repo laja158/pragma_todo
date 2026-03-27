5dae32d074798da2d621068f27661437f3c115e5
Jose Acosta <laja158@gmail.com>
2026-03-26 21:36:52 -0500

    feat: add local storage persistence for tasks

--- src/app/core/services/task.service.ts
+++ src/app/core/services/task.service.ts
@@ -6,13 +6,31 @@ import { Task } from '../models/task.model';
 export class TaskService {
 
   private tasks$ = new BehaviorSubject<Task[]>([]);
+  private STORAGE_KEY = 'tasks';
+
+  constructor() {
+    try {
+      const savedTasks = localStorage.getItem(this.STORAGE_KEY);
+      if (savedTasks) {
+        this.tasks$.next(JSON.parse(savedTasks));
+      }
+    } catch {
+      this.tasks$.next([]);
+    }
+  }
+
+  private save(tasks: Task[]) {
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(tasks));
+  }
 
   getTasks() {
     return this.tasks$.asObservable();
   }
 
   addTask(task: Task) {
-    this.tasks$.next([...this.tasks$.value, task]);
+    const updated = [...this.tasks$.value, task];
+    this.tasks$.next(updated);
+    this.save(updated);
   }
 
   toggleTask(id: string) {
@@ -20,9 +38,12 @@ export class TaskService {
       task.id === id ? { ...task, completed: !task.completed } : task
     );
     this.tasks$.next(updated);
+    this.save(updated);
   }
 
   deleteTask(id: string) {
-    this.tasks$.next(this.tasks$.value.filter(task => task.id !== id));
+    const updated = this.tasks$.value.filter(task => task.id !== id);
+    this.tasks$.next(updated);
+    this.save(updated);
   }
 }
